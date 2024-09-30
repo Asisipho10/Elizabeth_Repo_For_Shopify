1b2c365919403a1d40f75bc66280a700e87d5c30
Asisipho10 <asisipho@wbwr.io>
2024-09-30 12:16:09 +0200

    Hello Elizabeth

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './liquid/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './templates/customers/*.liquid',
+    './templates/*.liquid'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
